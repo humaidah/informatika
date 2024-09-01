a93fbd163f84b156c907810d84434b4b6eb20317
Muna Humaidah <145946442+humaidah@users.noreply.github.com>
2024-09-01 11:49:33 +0700

    Add files via upload

---
+++ latihan1_muna humaidah_python.py
@@ -0,0 +1,16 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "Berlibur di Jogja"
+print(Judul)
+
+#penulisan case
+method_name = "Muna Humaidah"
+print(method_name)
+
+#penulisan blok program
+a = 25
+b = 45
+if b > a:
+    print("b is greater than a")
